b974feb28e9fc18e25cfa7bc90199571bf4904f1
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-09-05 13:34:11 +0200

    Added class FlatWhite

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        /// <summary>
+        /// hvor meget mælk skal bruge i FlatWhite
+        /// </summary>
+        /// <returns></returns>
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        /// <summary>
+        /// prisen på en FlatWhite
+        /// </summary>
+        /// <returns></returns>
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        /// <summary>
+        /// styrken på en FlatWhite
+        /// </summary>
+        /// <returns></returns>
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
